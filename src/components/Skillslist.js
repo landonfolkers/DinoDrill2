0c1f341f11a8bd4b36191b4be48191d8e089b0a7
Landon Folkers <32934735+landonfolkers@users.noreply.github.com>
2018-04-20 11:44:32 -0600

    fixed skillslist

--- src/components/Skillslist.js
+++ src/components/Skillslist.js
@@ -4,9 +4,7 @@ export default function Skillslist({ profile }) {
     return <section className="skills-container">
         <h4>Skills</h4>
         <ul>
-            {profile.skills.map(skill => <li className="skills-list" key={skill}>
-                {skill}</li>
-            )}
+            {profile.skills.map(skill => <li className="skills-list" key={skill}>{skill}</li>)}
         </ul>
     </section>
 }
